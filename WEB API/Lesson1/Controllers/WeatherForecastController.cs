0bb23c424c56dced68840edc1e26ab382c0a7e1e
FW960 <artuninp@gmail.com>
2022-07-04 21:57:47 +0800

    Готовые запросы

--- WEB API/Lesson1/Controllers/WeatherForecastController.cs
+++ WEB API/Lesson1/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lesson1.Controllers
 {
@@ -16,40 +16,57 @@ namespace Lesson1.Controllers
         [HttpGet("Get")]
         public IActionResult Read()
         {
-            return Ok(_holder.values);
+            string total = "";
+
+            for (int i = 0; i < _holder.values.Count; i++)
+            {
+                total += @$"{_holder.values[i].Date.ToString()}. Celsius: {_holder.values[i].TemperatureC}. Fahrenheit: {_holder.values[i].TemperatureF}
+";
+            }
+
+            return Ok(total);
         }
-        [HttpPut("Put")]
-        public IActionResult Update([FromQuery] WeatherForecast toUpdate, WeatherForecast newValue)
+
+        [HttpPatch("Patch")]
+        public IActionResult Update([FromQuery] WeatherForecast toUpdate, int tempC)
         {
+
             for (int i = 0; i < _holder.values.Count; i++)
             {
                 if (toUpdate.Equals(_holder.values[i]))
                 {
-                    _holder.values[i] = newValue;
-                    return Ok();
+                    _holder.values[i].TemperatureC = tempC;
+
+                    return Ok($"Temp of date: {toUpdate} have been updated to {tempC}C and {_holder.values[i].TemperatureF}F.");
                 }
             }
 
-            return BadRequest();
+            return BadRequest("Date haven't been found.");
         }
+
         [HttpDelete("Delete")]
-        public IActionResult Delete([FromBody] WeatherForecast toDelete)
+        public IActionResult Delete([FromQuery] WeatherForecast toDelete)
         {
 
             if (_holder.values.Contains(toDelete))
             {
                 _holder.values.Remove(toDelete);
-                return Ok();
+                return Ok("Date have been deleted.");
             }
 
-            return BadRequest();
+            return BadRequest("Date haven't been found.");
         }
-        [HttpPost("Post")]
-        public IActionResult Post([FromBody] WeatherForecast toPost)
-        {
-            _holder.values.Add(toPost);
 
-            return Ok();
+        [HttpPost("Post")]
+        public IActionResult Post( WeatherForecast weatherForecast)
+        {
+            if (_holder.values.Contains(weatherForecast))
+                return BadRequest("Date with set properties already exists.");
+
+            _holder.values.Add(weatherForecast);
+
+            return Ok("Temperature have been added.");
         }
+
     }
 }
--- WEB API/Lesson1/WeatherForecast.cs
+++ WEB API/Lesson1/WeatherForecast.cs
@@ -1,16 +1,13 @@
 public class WeatherForecast
 {
-    public DateTime Date { get; set; }
+    public DateTime Date { get; internal set; }
 
-    public int TemperatureC { get; set; }
+    public int TemperatureC { get; internal set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
-
+    public int TemperatureF =>32 + (int)(this.TemperatureC / 0.5556);
     public override int GetHashCode()
     {
-        return TemperatureC.GetHashCode() + TemperatureF.GetHashCode();
+        return Date.GetHashCode();
     }
 
     public override bool Equals(object? weatherForecast)
